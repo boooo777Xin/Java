c923cefa2cae70267327ba30eb09e5024a84239f
yossy731 <yotta713daz@gmail.com>
2024-02-05 13:19:52 +0900

    commit

--- Test/src/Kadai/ExceptionInOutKadai/MoneyCount.java
+++ Test/src/Kadai/ExceptionInOutKadai/MoneyCount.java
@@ -4,42 +4,23 @@ public class MoneyCount {
 
     public static void main(String[] args) {
 
+        int[] moneyInt = { 10000, 5000, 1000, 500, 100, 50, 10, 5, 1 };
+        String[] moneyStr = { "一万円札", "五千円札", "千円札",
+                "五百円玉", "百円玉", "五十円玉",
+                "十円玉", "五円玉", "一円玉" };
+
+        System.out.print("金額を入力して下さい -> ");
         try {
 
             Scanner sin = new Scanner(System.in);
-            System.out.print("金額を入力して下さい -> ");
             int price = sin.nextInt();
-
-            System.out.println("一万円札:" + price / 10000 + "枚");
-            price %= 10000;
-
-            System.out.println("五千円札:" + price / 5000 + "枚");
-            price %= 5000;
-
-            System.out.println("千円札:" + price / 1000 + "枚");
-            price %= 1000;
-
-            System.out.println("五百円玉:" + price / 500 + "枚");
-            price %= 500;
-
-            System.out.println("百円玉:" + price / 100 + "枚");
-            price %= 100;
-
-            System.out.println("五十円玉:" + price / 50 + "枚");
-            price %= 50;
-
-            System.out.println("十円玉:" + price / 10 + "枚");
-            price %= 10;
-
-            System.out.println("五円玉:" + price / 5 + "枚");
-            price %= 5;
-
-            System.out.println("一円玉:" + price / 1 + "枚");
+            for (int i = 0; i < moneyInt.length; i++) {
+                System.out.println(moneyStr[i] + ":" + price / moneyInt[i] + "枚");
+                price %= moneyInt[i];
+            }
             sin.close();
-
         } catch (InputMismatchException e) {
             System.out.println("数字以外が入力されています");
         }
-
     }
 }
